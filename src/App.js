fe9fb3600568f54012b0fde6945ecc984975e9cd
et120 <e.trotter120@gmail.com>
2024-03-05 10:04:57 -0800

    square component style add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import BoardComponent from './components/Board/BoardComponent';
+import SquareComponent from './components/Square/SquareComponent';
 
 function App() {
   return (
     <div className="App">
-      <BoardComponent />
+      <SquareComponent />
     </div>
   );
 }
--- src/components/Square/SquareComponent.js
+++ src/components/Square/SquareComponent.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './SquareComponent.css'
 
 const SquareComponent = ( value, onClick) => {
   return (
     <button className='square' onClick={onClick}>
-      { value }
+      X
     </button>
   )
 }
